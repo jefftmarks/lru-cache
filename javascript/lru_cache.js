8c3f9ac7ee93270906e5485dc6fbc3c952d12a4a
Jeff Marks <jeff.t.marks@gmail.com>
2022-11-10 10:05:18 -0500

    done with assignment

--- javascript/lru_cache.js
+++ javascript/lru_cache.js
@@ -15,25 +15,71 @@ class DoublyLinkedList {
 
   // ADD THE NODE TO THE HEAD OF THE LIST
   addHead(node) {
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+      return;
+    }
 
+    this.head.prev = node;
+    node.next = this.head;
+    this.head = node;
+    this.head.prev = null;
   }
 
   // REMOVE THE TAIL NODE FROM THE LIST
   // AND RETURN IT
   removeTail() {
+    if (!this.tail) {
+      return this.tail;
+    }
 
+    if (!this.head.next) {
+      this.head = null;
+      return this.tail;
+    }
+
+    let oldTail = this.tail;
+    this.tail = this.tail.prev;
+    this.tail.next = null;
+    return oldTail;
   }
 
   // REMOVE THE GIVEN NODE FROM THE LIST
   // AND THEN RETURN IT
   removeNode(node) {
+    if (!this.head) {
+      return null;
+    }
 
+    let curr = this.head;
+
+    while (curr) {
+      if (curr === node) {
+        if (node === this.tail) {
+          this.removeTail();
+        } else if (node === this.head) {
+          this.head = node.next;
+          this.head.prev = null;
+        } else {
+          node.prev.next = node.next;
+          node.next.prev = node.prev;
+        }
+        return node;
+      } else {
+        curr = curr.next;
+      }
+    }
+
+    return null;
   }
 
   // MOVE THE GIVEN NODE FROM ITS LOCATION TO THE HEAD
   // OF THE LIST
   moveNodeToHead(node) {
-
+    if (this.removeNode(node)) {
+      this.addHead(node);
+    }
   }
 }
 
@@ -42,14 +88,20 @@ class LRUCache {
     this.limit = limit;
     this.size = 0;
     this.hash = {};
-    this.list = new DoublyLinkedList(limit);
+    this.list = new DoublyLinkedList();
   }
 
   // RETRIEVE THE NODE FROM THE CACHE USING THE KEY
   // IF THE NODE IS IN THE CACHE, MOVE IT TO THE HEAD OF THE LIST AND RETURN IT
   // OTHERWISE RETURN -1
   get(key) {
-
+    const node = this.hash[key];
+    if (!node) {
+      return -1;
+    } else {
+      this.list.moveNodeToHead(node);
+      return node;
+    }
   }
 
   // ADD THE GIVEN KEY AND VALUE TO THE CACHE
@@ -60,7 +112,21 @@ class LRUCache {
   // IF THE CACHE IS FULL, REMOVE THE LEAST RECENTLY USED ITEM BEFORE ADDING
   // THE NEW DATA TO THE CACHE
   put(key, value) {
-
+    if (this.hash[key]) {
+      const node = this.hash[key];
+      node.data = value;
+      this.list.moveNodeToHead(node);
+    } else {
+      if (this.size === this.limit) {
+        const oldTail = this.list.removeTail();
+        delete this.hash[oldTail.key];
+        --this.size;
+      }
+      const new_node = new Node(value, key);
+      this.hash[key] = new_node;
+      this.list.addHead(new_node);
+      ++this.size;
+    }
   }
 }
 
